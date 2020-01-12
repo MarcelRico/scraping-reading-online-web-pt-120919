80ea9e3fef4d0de3c63ac01d3e37f3f6c555ebd9
Marcel Rico <personal.mr4@gmail.com>
2020-01-12 18:01:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,14 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+flatiron_courses = []
+
+flatiron_course_list = doc.css("section#2a778efd-1685-5ec6-9e5a-0843d6a88b7b div.title-oE5vT4")
+flatiron_course_list.each{|course| flatiron_courses << course.text.strip}
+
+
+p flatiron_course_list[0].attributes.name
+
+
